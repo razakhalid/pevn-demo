1a63fac1130dd4a2866162235af5ca919a124f86
razakhalid <ra97za@gmail.com>
2024-03-14 18:14:10 -0500

    created vue app

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const host = 'http://localhost';
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Listening on ${host}:${port}`);
