73ba268244fbe0be90d84db26f10ec16cced9d90
razakhalid <ra97za@gmail.com>
2024-03-14 18:50:11 -0500

    updated path to dist dir

--- server/src/server.js
+++ server/src/server.js
@@ -5,10 +5,10 @@ const app = express();
 const host = 'http://localhost';
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
