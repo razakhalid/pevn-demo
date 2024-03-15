1fd789e75506b9fb61a7a6334c0f451ef9863aaa
razakhalid <ra97za@gmail.com>
2024-03-15 13:18:50 -0500

    removed hello world component

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,13 @@ const host = 'http://localhost';
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', '..', 'client', 'dist')));
+app.use(express.json());
 
+app.get('/quotes', (req, res) => {
+    res.json({
+        message: 'hello world'
+    });
+});
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
 });
